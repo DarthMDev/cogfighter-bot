2a3afddced7c868f80810a8161087f7115c40be8
DarthNihilus1 <10155689+DarthNihilus1@users.noreply.github.com>
2021-11-28 18:25:21 -0500

    sqliteFuncs: Add a set balance function

--- sqliteFuncs.py
+++ sqliteFuncs.py
@@ -20,6 +20,13 @@ def sub_balance(ctx, num):
     userBal -= num
     cur.execute("UPDATE users SET balance = :bal WHERE id = :id", {'bal': userBal, 'id': ctx.author.id})
     con.commit()
-
+    
+def set_balance(ctx, num):
+    cur.execute("SELECT * FROM users  WHERE id = :id", {'id': ctx.author.id})
+    userBal = cur.fetchone()[1]
+    userBal = num
+    cur.execute("UPDATE users SET balance = :bal WHERE id = :id", {'bal': userBal, 'id': ctx.author.id})
+    con.commit()
+ 
 # Subtract Balance test, Passed!
 # sub_balance(1, 150)
