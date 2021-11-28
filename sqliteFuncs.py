0744d50dc4e8dd7aedfc46d7bfd0d7135fbad484
Galaxy711 <66341738+Galaxy711@users.noreply.github.com>
2021-11-28 16:13:02 -0500

    sqliteFuncs: Create file and add the functions add_balance and sub_balance
    Create sqliteFuncs.py, which will contain functions to make database manipulation easy for future programming of the bot.
    
    add_balance: Takes 2 args, the context, and the amount of currency to add.
    
    sub_balance: same as add_balance... but subts the given num from the user's balance.

---
+++ sqliteFuncs.py
@@ -0,0 +1,25 @@
+import sqlite3
+from discord.ext import commands
+
+con = sqlite3.connect('users.db')
+cur = con.cursor()
+
+def add_balance(ctx, num):
+    cur.execute("SELECT * FROM users  WHERE id = :id", {'id': ctx.author.id})
+    userBal = cur.fetchone()[1]
+    userBal += num
+    cur.execute("UPDATE users SET balance = :bal WHERE id = :id", {'bal': userBal, 'id': ctx.author.id})
+    con.commit()
+
+# Add Balance test, Passed!
+# add_balance(1, 100)
+
+def sub_balance(id, num):
+    cur.execute("SELECT * FROM users  WHERE id = :id", {'id': id})
+    userBal = cur.fetchone()[1]
+    userBal -= num
+    cur.execute("UPDATE users SET balance = :bal WHERE id = :id", {'bal': userBal, 'id': id})
+    con.commit()
+
+# Subtract Balance test, Passed!
+# sub_balance(1, 150)
