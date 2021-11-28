0f6d6dc0974e8872af865c4fcb149272d99dc010
Galaxy711 <66341738+Galaxy711@users.noreply.github.com>
2021-11-28 16:14:50 -0500

    sqliteFuncs: Fix last commit
    forgot to change the args back from testing.

--- sqliteFuncs.py
+++ sqliteFuncs.py
@@ -14,11 +14,11 @@ def add_balance(ctx, num):
 # Add Balance test, Passed!
 # add_balance(1, 100)
 
-def sub_balance(id, num):
-    cur.execute("SELECT * FROM users  WHERE id = :id", {'id': id})
+def sub_balance(ctx, num):
+    cur.execute("SELECT * FROM users  WHERE id = :id", {'id': ctx.author.id})
     userBal = cur.fetchone()[1]
     userBal -= num
-    cur.execute("UPDATE users SET balance = :bal WHERE id = :id", {'bal': userBal, 'id': id})
+    cur.execute("UPDATE users SET balance = :bal WHERE id = :id", {'bal': userBal, 'id': ctx.author.id})
     con.commit()
 
 # Subtract Balance test, Passed!
