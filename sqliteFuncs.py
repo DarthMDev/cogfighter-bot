f957ff5354be03825a086815fe07717bfd71454c
Galaxy711 <66341738+Galaxy711@users.noreply.github.com>
2021-11-30 08:56:57 -0500

    sqliteFuncs: reformat and remove db clear

--- sqliteFuncs.py
+++ sqliteFuncs.py
@@ -11,14 +11,14 @@ def add_balance(id, num):
     A function that takes a user's id and a number 
     and adds that number to their balance.
     """
-    #reads database and gets the specified user's data
+    # reads database and gets the specified user's data
     cur.execute("SELECT * FROM users  WHERE id = :id", {'id': id})
-    #Fetches the balance of the specified user
+    # Fetches the balance of the specified user
     userBal = cur.fetchone()[1]
-    #Adds the amount specified in the argument to the varuable
+    # Adds the amount specified in the argument to the varuable
     userBal += num
     with con:
-        #Updates the user's balance , based on the id an the variable we assigned
+        # Updates the user's balance , based on the id an the variable we assigned
         cur.execute("UPDATE users SET balance = :bal WHERE id = :id", {'bal': userBal, 'id': id})
 
 
@@ -27,15 +27,15 @@ def sub_balance(id, num):
         A function that takes a user's id and a number 
         and subtracts that number from their balance.
     """
-    #reads database and gets the specified user's data
+    # reads database and gets the specified user's data
 
     cur.execute("SELECT * FROM users  WHERE id = :id", {'id': id})
-    #Fetches the balance of the specified user
+    # Fetches the balance of the specified user
     userBal = cur.fetchone()[1]
-    #Subtracts the amount specified in the argument to the varuable
+    # Subtracts the amount specified in the argument to the varuable
     userBal -= num
     with con:
-        #Updates the user's balance , based on the id an the variable we assigned
+        # Updates the user's balance , based on the id an the variable we assigned
         cur.execute("UPDATE users SET balance = :bal WHERE id = :id", {'bal': userBal, 'id': id})
 
 
@@ -64,12 +64,12 @@ def sub_crates(id, num):
 
 
 def add_cooldown(id, num):
-    #TODO create entry for author cooldown for the event then we can make these functions
-    pass #TODO
+    # TODO create entry for author cooldown for the event then we can make these functions
+    pass  # TODO
 
 
 def sub_cooldown(id, num):
-    pass #TODO
+    pass  # TODO
 
 
 def set_value(id, var, value):
@@ -87,6 +87,7 @@ def set_value(id, var, value):
     with con:
         cur.execute("UPDATE users SET {} = :value WHERE id = :id".format(var), {'value': value, 'id': id})
 
+
 def fetch_data(id, var):
     """
     Fetches data based on the id and which variable you want.
@@ -103,20 +104,13 @@ def fetch_data(id, var):
     cur.execute("SELECT {} FROM 'users' WHERE id = :id".format(var), {'id': id})
     return cur.fetchone()[0]
 
-def clear_db():
-    """
-    Wipes the database
-    RUN AT YOUR OWN RISK
-    """
+
 def create_user(id):
     """
     Creates a new user in the database based on the id specified
     default values:
     balance: 100, crates: 0, dailycooldown: 0, weeklycooldown: 0, inventory: '[]'
     """
-    #TODO in the future make it so if no id is specified make
-    # it so its based on the last id number in the database +1 
-    #I think it should be something built into sql but not sure
     with con:
         cur.execute("INSERT INTO users VALUES (:id, 100, 0, 0.0, 0.0, '[]')", {'id': id})
 
@@ -131,9 +125,10 @@ def does_user_exist(id):
     Function that checks if a user exists based on the id specified
     """
     cur.execute("SELECT * FROM users WHERE id = :id", {'id': id})
-    if cur.fetchone() is None: return False
-    else: return True
-
+    if cur.fetchone() is None:
+        return False
+    else:
+        return True
 
 # Leaving the next comment here for future reference while this file is being worked on.
 # cur.execute("CREATE TABLE users (id integer, balance integer, crates integer, dailycooldown real, weeklycooldown real, inventory text)")
