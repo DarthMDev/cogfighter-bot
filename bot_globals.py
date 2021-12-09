e6ec8c57597ee9d6457ab8a9f7aff2c7cf48314d
Galaxy711 <66341738+Galaxy711@users.noreply.github.com>
2021-12-09 04:02:05 -0500

    globals: Change pies to whole pies

--- bot_globals.py
+++ bot_globals.py
@@ -8,7 +8,8 @@ import json
 GAG_EMOS = ('<:cupcake:914821822875316224>', '<:fruitpieslice:914821822812409898>',
             '<:creampieslice:914821822598512702>', '<:fruitpie:914821822875320330>', '<:creampie:914821822229405726>',
             '<:bday:914821822715936788>', '<:wedding:914821822632067152>')
-GAGS = ('Cupcake', 'Fruit Pie Slice', 'Cream Pie Slice', 'Fruit Pie', 'Cream Pie', 'Birthday Cake', 'Wedding Cake')
+GAGS = ('Cupcake', 'Fruit Pie Slice', 'Cream Pie Slice', 'Whole Fruit Pie', 'Whole Cream Pie', 'Birthday Cake',
+        'Wedding Cake')
 GAG_DAMAGES = (5, 15, 25, 50, 75, 115, 165)
 
 # Cog constants
