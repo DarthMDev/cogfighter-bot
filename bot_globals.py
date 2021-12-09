05b7661f086be7b46658ca6a9655d355670b558c
Galaxy711 <66341738+Galaxy711@users.noreply.github.com>
2021-12-09 03:31:38 -0500

    Globals: Turn non-changing lists into tuples
    tuples are much more efficient if you're not changing the information inside of them. Feel free to revert this change if necessary.

--- bot_globals.py
+++ bot_globals.py
@@ -5,13 +5,23 @@ import json
 
 # Global Variables
 # Gag constants
-GAG_EMOS = ['<:cupcake:914821822875316224>', '<:fruitpieslice:914821822812409898>',
+GAG_EMOS = ('<:cupcake:914821822875316224>', '<:fruitpieslice:914821822812409898>',
             '<:creampieslice:914821822598512702>', '<:fruitpie:914821822875320330>', '<:creampie:914821822229405726>',
-            '<:bday:914821822715936788>', '<:wedding:914821822632067152>']
-GAGS = ['Cupcake', 'Fruit Pie Slice', 'Cream Pie Slice', 'Fruit Pie', 'Cream Pie', 'Birthday Cake', 'Wedding Cake']
-THROW_DAMAGES = [5, 15, 25 , 50, 75, 115, 165, 225, 295, 375]
+            '<:bday:914821822715936788>', '<:wedding:914821822632067152>')
+GAGS = ('Cupcake', 'Fruit Pie Slice', 'Cream Pie Slice', 'Fruit Pie', 'Cream Pie', 'Birthday Cake', 'Wedding Cake')
+GAG_DAMAGES = (5, 15, 25, 50, 75, 115, 165)
 
 # Cog constants
+SUIT_NAMES = (
+'Cold Caller', 'Telemarketer', 'Name Dropper', 'Glad Hander', 'Mover and Shaker', 'Two Face', 'Mingler', 'Mr Hollywood')
+SUIT_IMAGES = ('https://cdn.discordapp.com/attachments/917177481847521300/917177636676059146/262.png',
+               'https://cdn.discordapp.com/attachments/917177481847521300/917177733161832528/139.png',
+               'https://cdn.discordapp.com/attachments/917177481847521300/917177763243384922/203.png',
+               'https://cdn.discordapp.com/attachments/917177481847521300/917177804716638208/256.png',
+               'https://cdn.discordapp.com/attachments/917177481847521300/917177835016323072/189.png',
+               'https://cdn.discordapp.com/attachments/917177481847521300/917177864619700224/238.png',
+               'https://cdn.discordapp.com/attachments/917177481847521300/917177910073389056/248.png',
+               'https://cdn.discordapp.com/attachments/917177481847521300/917177955287969812/244.png')
 
 #Bot Variables
 with open('config/config.json') as file:
